d53650c526cad078cf0b4cb752ab01955e46f5c2
LILIS <lilis@skyremix.com>
2016-08-19 09:46:58 +0900

    Rename 20160805_ETC에도_잘_될까?.php to 20160805_ETC에도_잘_될까.php

--- etc/20160805_ETC에도_잘_될까?.php
+++
@@ -1,3 +0,0 @@
-<span class="card-title">ETC에도 잘 될까?</span>
-<p class="grey-text">2016-08-05</p>
-<p>&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!&nbsp;제발 잘 좀 되길!</p>
